e53e5cfa79c90f39662d58f4a968e690d3a4ef88
Ed Travalia <33702136+etravalia@users.noreply.github.com>
2019-09-19 18:05:45 -0700

    Grading comments
    Very good work.   24/25

--- src/OldLady.java
+++ src/OldLady.java
@@ -1,4 +1,24 @@
-
+/***
+ * Grading Comments:
+ * Very good job.  Definitely got the idea of 
+ * reducing redundancy, and your use of nested
+ * methods (inside other methods) is key.
+ *  Compiles and Runs:  15/15
+ *  Desired Output:  6/6
+ *  Consistency:  2/2.   I see what you did with indenting the 'space'
+ *    lines - and that's ok - but we'd probably rather see you use a
+ *    new line instead of indenting - as indenting really represents
+ *    a change in code flow rather than output.
+ *  Efficiency: 1/2:   The nesting is great, but you could have
+ *    taken this a couple steps further to reduce your bulk typing
+ *    and duplication.   A method that just does:
+ *    System.out.print("There was an old lady who swallowed a");
+ *    could be called followed by:
+ *    System.out.println("fly,");  ..(spider, bird etc)
+ *    and you could've taken a step further by separating versus
+ *    but what you did overall was really great.
+ *  24/25
+ */
 public class OldLady {
 
 	public static void main(String[] args) {
